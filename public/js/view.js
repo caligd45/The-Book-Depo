0325dbeb431f8542466e4699bcb25c0a620655a3
Lady Stock <nikki.castlea@gmail.com>
2020-08-07 14:02:47 -0600

    added category dropdown for search bar

--- public/js/view.js
+++ public/js/view.js
@@ -1,72 +1,99 @@
-// When user hits the search-btn
-$("#search-btn").on("click", function(event) {
-  event.preventDefault();
+$(document).ready(function () {
+  //Variables
+  // var instance = M.FormSelect.getInstance(elem);
 
-  // Save the book they typed into the book-search input
-  var bookSearched = $("#book-search").val().trim();
+  // Function Calls 
+  $('select').formSelect();
+  // instance.getSelectedValues();
 
-  // Make an AJAX get request to our api, including the user's book in the url
-  $.get("/api/" + bookSearched, function(data) {
-
-    console.log(data);
-    // Call our renderBooks function to add our books to the page
-    renderBook(data);
-
-  });
-
-});
-
-// When user hits the author-search-btn
-$("#author-search-btn").on("click", function() {
-
-  // Save the author they typed into the author-search input
-  var authorSearched = $("#author-search").val().trim();
-
-  // Make an AJAX get request to our api, including the user's author in the url
-  $.get("/api/author/" + authorSearched, function(data) {
-
-    // Log the data to the console
-    console.log(data);
-    // Call our renderBooks function to add our books to the page
-    renderBook(data);
-
-  });
-
-});
-
-// When user hits the genre-search-btn
-$("#genre-search-btn").on("click", function() {
-
-  // Save the book they typed into the genre-search input
-  var genreSearched = $("#genre-search").val().trim();
-
-  // Make an AJAX get request to our api, including the user's genre in the url
-  $.get("/api/genre/" + genreSearched, function(data) {
-
-    console.log(data);
-    // Call our renderBooks function to add our books to the page
-    renderBook(data);
-
-  });
-
-});
-
-function renderBook(data) {
-  if (data.length !== 0) {
-
-    $("#stats").empty();
-    $("#stats").show();
-
-    for (var i = 0; i < data.length; i++) {
-
-      var div = $("<div>");
-
-      div.append("<h2>" + data[i].title + "</h2>");
-      div.append("<p>Author: " + data[i].author + "</p>");
-      div.append("<p>Genre: " + data[i].genre + "</p>");
-     
-      $("#stats").append(div);
-
-    }
+  if ($(".category") === "Book") {
+    bookSearch();
+  } else if ($(".category") === "Author") {
+    authorSearch();
+  } else {
+    genreSearch();
   }
-}
+
+  // Functions 
+    //search by book
+  const bookSearch = () => {
+
+    $("#search-btn").on("submit", function (event) {
+      event.preventDefault();
+
+      // Save the book they typed into the book-search input
+      const bookSearched = $(".query").val().trim();
+
+      // Make an AJAX get request to our api, including the user's book in the url
+      $.get("/api/" + bookSearched, function (data) {
+
+        console.log(data);
+        // Call our renderBooks function to add our books to the page
+        renderBook(data);
+
+      });
+
+    });
+  }
+
+  // Search by author
+  const authorSearch = () => {
+    $("#search-btn").on("submit", function () {
+
+      // Save the author they typed into the author-search input
+      const authorSearched = $("#author-search").val().trim();
+
+      // Make an AJAX get request to our api, including the user's author in the url
+      $.get("/api/author/" + authorSearched, function (data) {
+
+        // Log the data to the console
+        console.log(data);
+        // Call our renderBooks function to add our books to the page
+        renderBook(data);
+
+      });
+
+    });
+
+  }
+
+  // Search by genre
+  const genreSearch = () => {
+    $("#genre-search-btn").on("submit", function () {
+
+      // Save the book they typed into the genre-search input
+      const genreSearched = $("#genre-search").val().trim();
+
+      // Make an AJAX get request to our api, including the user's genre in the url
+      $.get("/api/genre/" + genreSearched, function (data) {
+
+        console.log(data);
+        // Call our renderBooks function to add our books to the page
+        renderBook(data);
+
+      });
+
+    });
+
+  }
+
+  function renderBook(data) {
+    if (data.length !== 0) {
+
+      $("#stats").empty();
+      $("#stats").show();
+
+      for (let i = 0; i < data.length; i++) {
+
+        const div = $("<div>");
+
+        div.append("<h2>" + data[i].title + "</h2>");
+        div.append("<p>Author: " + data[i].author + "</p>");
+        div.append("<p>Genre: " + data[i].genre + "</p>");
+
+        $("#stats").append(div);
+
+      }
+    }
+  };
+})
