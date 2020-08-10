6e7fa794afe67f0567b8f40006a5f433bfd96ea2
Lady Stock <nikki.castlea@gmail.com>
2020-08-10 16:48:59 -0600

    styling on book cards

--- public/js/script.js
+++ public/js/script.js
@@ -1,69 +1,71 @@
 $(document).ready(() => {
-    
-    $("#submitBtn").on("click", ()=>{
+
+    $("#submitBtn").on("click", (event) => {
+
         const searchValue = $("#search-btn").val();
 
         $("#search-btn").val("");
-
         searchBook(searchValue);
+
+
     });
 
-    function searchBook(searchValue){
+    function searchBook(searchValue) {
         $.ajax({
             type: "GET",
             url: `https://www.googleapis.com/books/v1/volumes?q=${searchValue}`,
             dataType: "json"
         }).then((response) => {
-                console.log (response);
-                renderBook(response);
-        }); 
+            console.log(response);
+            renderBook(response);
+        });
     }
 
+
     function renderBook(data) {
-        if (data.length !== 0) {   
+        if (data.length !== 0) {
 
-          $("#stats").empty();
-          $("#stats").show();
+            $("#stats").empty();
+            $("#stats").show();
 
-        console.log(data.items[0].volumeInfo.title);
-        
-        
-        for (let i = 0; i < data.items.length; i++) {
-            
-      
-            const res = data.items[i].volumeInfo;
-            console.log(res);
-            
-            const cardDiv = $("<div class='col s12 m6 l4 xl3' id='bookSearchResults'>")
-            const bookCard = $("<div class='card'> ");
-            const imgDiv = $("<div class= 'card-image'>");
-            const bookImage = $(`<img class='imgOfbook' src=''/>`);
-            const bookTitle = $(`<span class = 'card-title'>${res.title}<span>`);
-            const desDiv = $("<div class='card-content'>");
-            const description = $(`<p> ${res.description}</p>`);
-            const saveBtn = $("<a class='waves-effect waves-light btn-small saveBook'>Save This Book</a>")
-      
-            if(!res.imageLinks.smallThumbnail) {
-                imgSrc = "./images/defaultBook.png"
-            } else {
-                imgSrc = res.imageLinks.smallThumbnail;
+            console.log(data.items[0].volumeInfo.title);
+
+
+            for (let i = 0; i < data.items.length; i++) {
+
+
+                const res = data.items[i].volumeInfo;
+                console.log(res);
+
+                const cardDiv = $("<div class='col s12 m6 l4 xl3' id='bookSearchResults'>")
+                const bookCard = $("<div class='card bookCard'> ");
+                const imgDiv = $("<div class= 'card-image'>");
+                const bookImage = $(`<img class='imgOfbook' src=''/>`);
+                const desDiv = $("<div class='card-content'>");
+                const description = $(`<p> ${res.description}</p>`);
+                const saveBtn = $("<a class='waves-effect waves-light btn-small saveBook'>Save This Book</a>")
+
+                if (!res.imageLinks.thumbnail) {
+                    imgSrc = "./images/defaultBook.png"
+                } else {
+                    imgSrc = res.imageLinks.thumbnail;
+                }
+
+                bookImage.attr("src", imgSrc)
+
+                bookCard.append(`<h2 class='card-title'>  ${res.title} </h2>`);
+                bookCard.append(`<p class="author">Author: ${res.authors}</p>`);
+
+                imgDiv.append(bookImage);
+                desDiv.append(description);
+                bookCard.append(imgDiv, desDiv, saveBtn);
+                cardDiv.append(bookCard);
+
+                console.log(cardDiv);
+
+                $("#stats").append(cardDiv);
+
             }
-
-            bookImage.attr("src", imgSrc)
-
-            bookCard.append(`<h2 class='card-title'>  ${res.title} </h2>`);
-            bookCard.append(`<p>Author: ${res.authors}</p>`);
-           
-            imgDiv.append(bookImage);
-            desDiv.append(description);
-            bookCard.append(imgDiv, desDiv, saveBtn);
-            cardDiv.append(bookCard);
-
-            console.log(cardDiv);
-            
-            $("#stats").append(cardDiv);
-      
-          }
         }
-      };
+    };
 })
