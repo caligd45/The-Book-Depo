c0279b6926c1c51c62056205cc552811ea8dc258
Lady Stock <nikki.castlea@gmail.com>
2020-08-10 14:20:33 -0600

    book search renders to page

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
+    
     $("#submitBtn").on("click", ()=>{
-        var searchValue = $("#search-btn").val();
+        const searchValue = $("#search-btn").val();
 
         $("#search-btn").val("");
 
@@ -14,7 +15,55 @@ $(document).ready(() => {
             dataType: "json"
         }).then((response) => {
                 console.log (response);
-                $(".welcome").text("Book Title: " + response.items[0].volumeInfo.title + " Author: " + response.items[0].volumeInfo.authors + " Genre: " + response.items[0].volumeInfo.categories[0]);
+                renderBook(response);
         }); 
     }
+
+    function renderBook(data) {
+        if (data.length !== 0) {   
+
+          $("#stats").empty();
+          $("#stats").show();
+
+        console.log(data.items[0].volumeInfo.title);
+        
+        
+        for (let i = 0; i < data.items.length; i++) {
+            
+      
+            const res = data.items[i].volumeInfo;
+            console.log(res);
+            
+            const cardDiv = $("<div class='col s12 m6 l4 xl3' id='bookSearchResults'>")
+            const bookCard = $("<div class='card'> ");
+            const imgDiv = $("<div class= 'card-image'>");
+            const bookImage = $(`<img class='imgOfbook' src=''/>`);
+            const bookTitle = $(`<span class = 'card-title'>${res.title}<span>`);
+            const desDiv = $("<div class='card-content'>");
+            const description = $(`<p> ${res.description}</p>`);
+            const saveBtn = $("<a class='waves-effect waves-light btn-small saveBook'>Save This Book</a>")
+      
+            if(!res.imageLinks.smallThumbnail) {
+                imgSrc = "./images/defaultBook.png"
+            } else {
+                imgSrc = res.imageLinks.smallThumbnail;
+            }
+
+            bookImage.attr("src", imgSrc)
+
+            bookCard.append(`<h2 class='card-title'>  ${res.title} </h2>`);
+            bookCard.append(`<p>Author: ${res.authors}</p>`);
+           
+            imgDiv.append(bookImage);
+            desDiv.append(description);
+            bookCard.append(imgDiv, desDiv, saveBtn);
+            cardDiv.append(bookCard);
+
+            console.log(cardDiv);
+            
+            $("#stats").append(cardDiv);
+      
+          }
+        }
+      };
 })
